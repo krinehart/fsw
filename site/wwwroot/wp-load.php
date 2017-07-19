9e110cdc3dfe1578716a40a4982c37e667fcd408
krinehart <kyle@kylerinehart.com>
2017-07-19 10:20:47 -0700

    Update wp-load.php

--- site/wwwroot/wp-load.php
+++ site/wwwroot/wp-load.php
@@ -13,7 +13,7 @@
  * directory to allow the WordPress directory to remain
  * untouched.
  *
- * @package WordPress
+ * @package WordPress test
  */
 
 /** Define ABSPATH as this file's directory */
